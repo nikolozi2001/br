073906601de2506285455da52b63ca83df71b414
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-06-23 10:34:44 +0400

    feat: update Footer component to display dynamic current year and enhance styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,33 +1,40 @@
 import PropTypes from "prop-types";
 
 const Footer = ({ isEnglish }) => {
+  const currentYear = new Date().getFullYear();
+
   const content = {
     en: {
-      copyright: "© 2025 All rights reserved.",
+      copyright: `© ${currentYear} All rights reserved.`,
       organization: "National Statistics Office of Georgia (Geostat)",
       terms: "Terms of Data Usage",
       email: "Email:",
       phone: "Tel.:",
+      phoneDetails: "(internal: 214, 218, 219)",
     },
     ka: {
-      copyright: "© 2025 ყველა უფლება დაცულია.",
+      copyright: `© ${currentYear} ყველა უფლება დაცულია.`,
       organization: "საქართველოს სტატისტიკის ეროვნული სამსახური (საქსტატი)",
       terms: "მონაცემთა გამოყენების პირობები",
       email: "ელ. ფოსტა:",
       phone: "ტელ.:",
+      phoneDetails: "(შიდა: 214, 218, 219)",
     },
   };
 
   const t = isEnglish ? content.en : content.ka;
   return (
-    <footer className="bg-white/80 backdrop-blur-md text-sm text-gray-700 py-6 px-4 font-bpg-nino font-bold">
+    <footer className="bg-white/50 mt-3 text-sm text-gray-700 py-6 px-4 font-bpg-nino font-bold">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between gap-4">
         {/* Left side */}
         <div>
           <p className="mb-2">{t.copyright}</p>
           <p className="mb-2">{t.organization}</p>
           <p>
-            <a href="#" className="text-blue-600 hover:text-blue-800 hover:underline">
+            <a
+              href="#"
+              className="text-blue-600 hover:text-blue-800 hover:underline"
+            >
               {t.terms}
             </a>
           </p>
@@ -41,7 +48,9 @@ const Footer = ({ isEnglish }) => {
               br@geostat.ge
             </a>
           </p>
-          <p>{t.phone} (+995 032) 2 36-72-10 (შიდა: 214, 218, 219)</p>
+          <p>
+            {t.phone} (+995 032) 2 36-72-10 {t.phoneDetails}
+          </p>
         </div>
       </div>
     </footer>
