3e4ca80545e47d5fc8c86c4b65764e22911ef927
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-02-26 09:04:52 +0400

    feat: update API proxy target to use production URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://br-api.geostat.ge',
-        target: 'http://localhost:5001',
+        target: 'https://br-api.geostat.ge',
+        // target: 'http://localhost:5001',
         changeOrigin: true,
         secure: true,
       }
